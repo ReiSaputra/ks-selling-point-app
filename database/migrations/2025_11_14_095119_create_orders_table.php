1b2e574403a10af325214d4f3d288943e5168994
Muhrizky435 <Muhrizky435@gmail.com>
2025-11-17 10:05:55 +0700

    chore: setup .env and app key

---
+++ database/migrations/2025_11_14_095119_create_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('invoice_number')->unique();
+            $table->string('marketplace_invoice')->nullable();
+            $table->string('channel')->nullable();
+            $table->decimal('subtotal', 15, 2)->default(0);
+            $table->decimal('discount', 15, 2)->default(0);
+            $table->decimal('shipping_cost', 15, 2)->default(0);
+            $table->string('order_status')->nullable();
+            $table->string('expedition')->nullable();
+            $table->string('shipping_receipt')->nullable();
+            $table->text('note')->nullable();
+            $table->json('coupon')->nullable();
+            $table->decimal('admin_fee', 15, 2)->default(0);
+            $table->decimal('vat', 15, 2)->default(0);
+            $table->decimal('total', 15, 2)->default(0);
+            $table->string('payment_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
