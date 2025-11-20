edcca2355ca186cd9294130064db71d9cd07ebce
Muhammad Fathurraihan Saputra <121397266+ReiSaputra@users.noreply.github.com>
2025-11-20 10:55:53 +0700

    feat: report all done

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -8,24 +8,49 @@
     <div class="sidebar-wrapper">
         <nav class="mt-2">
             <ul class="nav sidebar-menu flex-column" data-lte-toggle="treeview">
+
                 <li class="nav-item">
                     <a href="{{ route('order-list') }}" class="nav-link">
-                        <i class="nav-icon bi bi-circle"></i>
+                        <i class="nav-icon bi bi-card-list"></i>
                         <p>Order List</p>
                     </a>
                 </li>
+
                 <li class="nav-item">
                     <a href="{{ route('upload-csv') }}" class="nav-link">
-                        <i class="nav-icon bi bi-circle"></i>
+                        <i class="nav-icon bi bi-file-earmark-arrow-up"></i>
                         <p>Upload CSV</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{ route('report') }}" class="nav-link">
-                        <i class="nav-icon bi bi-circle"></i>
-                        <p>Report</p>
+
+                <li class="nav-item has-treeview">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon bi bi-bar-chart"></i>
+                        <p>
+                            Report
+                            <i class="end bi bi-chevron-right"></i>
+                        </p>
                     </a>
+
+                    <ul class="nav nav-treeview">
+
+                        <li class="nav-item">
+                            <a href="{{ route('report.channel') }}" class="nav-link">
+                                <i class="nav-icon bi bi-diagram-3"></i>
+                                <p>Report per Channel</p>
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="{{ route('report.product') }}" class="nav-link">
+                                <i class="nav-icon bi bi-box-seam"></i>
+                                <p>Report per Product</p>
+                            </a>
+                        </li>
+
+                    </ul>
                 </li>
+
             </ul>
         </nav>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,10 @@ Route::middleware(["auth"])->group(function () {
     Route::get("/upload-csv", "UploadCSVController@show")->name("upload-csv");
     Route::post("/upload-csv", "UploadCSVController@perform")->name("upload-csv.perform");
 
-    Route::get("/report", "ReportController@show")->name("report");
+    Route::get("/report/channel", "ReportController@showChannel")->name("report.channel");
+    Route::get("/report/channel/export", "ReportController@exportPerChannel")->name("report.channel.export");
+    Route::get("/report/product", "ReportController@showProduct")->name("report.product");
+    Route::get("/report/product/export", "ReportController@exportPerProduct")->name("report.product.export");
+
+    Route::get("/logout", "AuthController@logout")->name("logout");
 });
